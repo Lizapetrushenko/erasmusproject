092a0e3d10c8d0b8354aa59b386f436033c49094
BBakkerr <beabakkerr@gmail.com>
2026-09-18 12:58:29 +0200

    I added some files for JS, because for setting up the frontend page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,3 @@
 import './bootstrap';
+import './authentication/login';
+import './authentication/register';
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+require __DIR__.'/auth.php';
