beac8baaea3f3e7d9c7420c2cae7c9d0ab2bf9a0
Lizapetrushenko <liza2511liza@gmail.com>
2026-09-22 11:12:48 +0200

    Added some routes for scores

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\QuizController;
+use App\Http\Controllers\Api\ScoreController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/quizzes', [QuizController::class, 'index']);
@@ -9,3 +10,7 @@ Route::get('/quizzes/{quiz}', [QuizController::class, 'show']);
 Route::post('/quizzes/{quiz}/start', [QuizController::class, 'start']);
 Route::post('/quizzes/{quiz}/submit', [QuizController::class, 'submit']);
 Route::get('/leaderboard', [QuizController::class, 'leaderboard']);
+
+Route::get('/scores', [ScoreController::class, 'index']);
+Route::post('/scores', [ScoreController::class, 'store']);
+Route::get('/scores/{score}', [ScoreController::class, 'show']);
