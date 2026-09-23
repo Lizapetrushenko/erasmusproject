5c591fb62ca6fb67f5fee520019f70a4cb0dd331
Bartul274 <bartul.trnjar@gmail.com>
2026-09-23 08:25:11 +0200

    FIxing merge conflict

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,24 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('app');
-});
+    return view('welcome');
+})->name('home');
 
 Route::get('/dashboard', function () {
-    return view('app');
+    return view('pages.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/login', function () {
-    return view('app');
+    return view('authentication.login');
 })->name('login');
 
+Route::post('/login', [AuthController::class, 'login'])->name('login.store');
+
 Route::get('/register', function () {
-    return view('app');
+    return view('authentication.register');
 })->name('register');
 
 Route::get('/questions', function () {
@@ -23,6 +26,8 @@ Route::get('/questions', function () {
 })->name('questions');
 
 
+Route::post('/register', [AuthController::class, 'register'])->name('register.store');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
 
 if (file_exists(__DIR__.'/auth.php')) {
     require __DIR__.'/auth.php';
